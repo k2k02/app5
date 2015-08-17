754c884a0d699e2d7e9d6c5f73ffb1cecea5ec5b
wkk91193 <wkk91193@yahoo.com>
2015-08-17 09:28:22 +0530

    first commit

---
+++ app-2/routes/fields.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var express=require('express');
+var router =express.Router();
+
+
+router.get('/fieldlist',function(req,res){
+   var db=req.db;
+   var collection=db.get('domlist');
+   collection.find({},{},function(e,docs){
+      res.json(docs); 
+   });  
+});
+
+module.exports=router;
